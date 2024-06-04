a4df323f97b668c39d319c175188d85960017dda
Anna Hnatiuk <losers2004@gmail.com>
2024-06-04 23:48:30 +0200

    CPP_SR1

---
+++ MauiApp1/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauiApp1
+{
+    internal class Calculator
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int[] Numbers { get; set; }
+
+        public int ProductOfMultiplesOf6()
+        {
+            int product = 1;
+            bool hasMultiple = false;
+            for (int i = A; i <= B; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    product *= i;
+                    hasMultiple = true;
+                }
+            }
+            return hasMultiple ? product : 0;
+        }
+
+        public int ProductInRange()
+        {
+            int prod = 1;
+            int count = 0;
+           
+            for (int i = 0; i < 3; i++)
+            {
+                if (Numbers[i] > 10 && Numbers[i] < 15)
+                {
+                    prod = prod * Numbers[i];
+                    count++;
+                }
+            }
+
+            if(count >1) return prod ;
+            else return 0;
+        }
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        Calculator calculator = new Calculator();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateMultiplesOf6()
+        {
+            try
+            {
+                calculator.A = int.Parse(entryA1.Text);
+                calculator.B = int.Parse(entryB1.Text);
+                int result = calculator.ProductOfMultiplesOf6();
+                resultLabel1.Text = $"Добуток чисел, кратних 6: {result}";
+            }
+            catch (Exception ex)
+            {
+                resultLabel1.Text = $"Помилка: {ex.Message}";
+            }
+        }
+
+        private void CalculateInRange()
+        {
+            try
+            {
+                calculator.Numbers = new int[]
+                {
+                    int.Parse(entry1.Text),
+                    int.Parse(entry2.Text),
+                    int.Parse(entry3.Text)
+                };
+                int result = calculator.ProductInRange();
+                resultLabel2.Text = $"Добуток чисел в діапазоні (10, 15): {result}";
+            }
+            catch (Exception ex)
+            {
+                resultLabel2.Text = $"Помилка: {ex.Message}";
+            }
+        }
+
+        private void CalculateMultiplesOf6Btn_Clicked(object sender, EventArgs e)
+        {
+            CalculateMultiplesOf6();
+        }
+
+        private void CalculateInRangeBtn_Clicked(object sender, EventArgs e)
+        {
+            CalculateInRange();
+        }
+    }
+}
